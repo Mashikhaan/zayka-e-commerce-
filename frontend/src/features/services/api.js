37f785e110a55b10901aa5382c4b03845d9e9a64
Mashi Khan <mashikhanwork@gmail.com>
2026-05-04 18:31:45 +0530

    use another method env setup so that backend can work

--- frontend/src/features/services/api.js
+++ frontend/src/features/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://zayka-e-commerce.onrender.com/api",
+   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true, // 👈 MOST IMPORTANT
 });
 
